d74ad604a97f66a8367ea5ad8dd4d732eb7d1b23
Javier Canillas <javier.canillas@recargapay.com>
2021-11-05 12:48:30 -0300

    More examples

--- src/test/java/org/techtalks/testing/argumentCaptor/MessageServiceTest.java
+++ src/test/java/org/techtalks/testing/argumentCaptor/MessageServiceTest.java
@@ -3,6 +3,8 @@ package org.techtalks.testing.argumentCaptor;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
+import org.mockito.ArgumentMatcher;
+import org.mockito.ArgumentMatchers;
 import org.mockito.Captor;
 import org.mockito.Mock;
 
@@ -35,8 +37,8 @@ class MessageServiceTest {
     }
 
     @Test
-    void messageDeliveredVerifyInternals() {
-        doReturn(true).when(platform).deliver(messageArgumentCaptor.capture());
+    void messageDeliveredAssertInternals() {
+        doReturn(true).when(platform).deliver(any());
         var service = new MessageService(platform);
         var to = "John";
         var subject = "Hello there!";
@@ -44,12 +46,40 @@ class MessageServiceTest {
 
         assertTrue(service.send(to, subject, body, false));
 
-        verify(platform, times(1)).deliver(any());
+        verify(platform, times(1)).deliver(messageArgumentCaptor.capture());
 
         var capturedMessage = messageArgumentCaptor.getValue();
         assertEquals(to, capturedMessage.getTo());
         assertEquals(subject, capturedMessage.getSubject());
         assertEquals(body, capturedMessage.getBody());
+        assertEquals(Platform.BodyFormat.PLAIN, capturedMessage.getBodyFormat());
+
+        verifyNoMoreInteractions(platform);
+    }
+
+    @Test
+    void messageDeliveredVerifyInternals() {
+        var service = new MessageService(platform);
+        final var to = "John";
+        final var subject = "Hello there!";
+        final var body = "When do you have time for a barbacue?";
+
+        var messageMatcher = new ArgumentMatcher<Platform.Message>() {
+
+            @Override
+            public boolean matches(Platform.Message argument) {
+                return to.equals(argument.getTo())
+                        && subject.equals(argument.getSubject())
+                        && body.equals(argument.getBody());
+            }
+        };
+        doReturn(true).when(platform).deliver(ArgumentMatchers.argThat(messageMatcher));
+
+        assertTrue(service.send(to, subject, body, false));
+
+        verify(platform, times(1)).deliver(messageArgumentCaptor.capture());
+
+        var capturedMessage = messageArgumentCaptor.getValue();
         assertEquals(Platform.BodyFormat.PLAIN, capturedMessage.getBodyFormat());
 
         verifyNoMoreInteractions(platform);
